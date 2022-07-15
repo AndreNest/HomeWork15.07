2b64cfe0645a6af539ecf8a54948845623c2de28
AndreyNesterov <1@nestandre.ru>
2022-07-16 00:16:58 +0300

    add Task 43

---
+++ Task 43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task 43 Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+void geetCoordinates(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y1 = k1 * x + b1;
+    double y2 = k2 * x + b2;
+    Console.WriteLine("Точка пересечения: " + y1 + ";" + y2);
+}
+geetCoordinates(Convert.ToDouble(Console.ReadLine()),
+                Convert.ToDouble(Console.ReadLine()),
+                Convert.ToDouble(Console.ReadLine()),
+                Convert.ToDouble(Console.ReadLine()));
+
+
+
+
+
+
+
--- Task41/Program.cs
+++ Task41/Program.cs
@@ -3,15 +3,25 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите число M: ");
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] randomArr = new int[m];
+// int numb = 0;
+// for (int i = 0; i < m; i++)
+// {
+//     randomArr[i] = new Random().Next(-10, 11);
+//     Console.Write(randomArr[i] + " ");
+//     if(randomArr[i] > 0) numb++;
+// }
+// Console.WriteLine("---------------------");
+// Console.WriteLine(numb);
+
+Console.Write("Сколько чисел вы хотите ввести?: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[] randomArr = new int[m];
 int numb = 0;
 for (int i = 0; i < m; i++)
 {
-    randomArr[i] = new Random().Next(-10, 11);
-    Console.Write(randomArr[i] + " ");
-    if(randomArr[i] > 0) numb++;
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    if (numbers > 0) numb++;
 }
-Console.WriteLine("---------------------");
-Console.WriteLine(numb);
+Console.WriteLine("Количество положительных чисел " + numb);
