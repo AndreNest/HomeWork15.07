fcff574d54c5b9317ebb506d7b81d7e9d0b754b1
AndreyNesterov <1@nestandre.ru>
2022-07-15 23:22:03 +0300

    add Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] randomArr = new int[m];
+int numb = 0;
+for (int i = 0; i < m; i++)
+{
+    randomArr[i] = new Random().Next(-10, 11);
+    Console.Write(randomArr[i] + " ");
+    if(randomArr[i] > 0) numb++;
+}
+Console.WriteLine("---------------------");
+Console.WriteLine(numb);
